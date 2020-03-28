51dbd967d540a582a8ac6ce2498e82f3e3540676
Masatoshi Suzuki <sappukei.suzuki@gmail.com>
2020-03-28 12:45:33 +0900

    Remove debugging

--- extract_wikipedia2vec_entity_embedding.py
+++ extract_wikipedia2vec_entity_embedding.py
@@ -10,15 +10,13 @@ ENTITY_MARKER = 'ENTITY/'
 def main(args):
     output_lines = []
     with bz2.open(args.input_file, 'rt') as f:
-        vocab_size, _ = map(int, f.readline().rstrip('\n').split(' '))
+        vocab_size, n_dim = map(int, f.readline().rstrip('\n').split(' '))
         for line in tqdm(f, total=vocab_size):
             if line[:len(ENTITY_MARKER)] == ENTITY_MARKER:
                 output_lines.append(line[len(ENTITY_MARKER):])
 
-            if len(output_lines) > 1234:
-                break
-
     with bz2.open(args.output_file, 'wt') as fo:
+        print(vocab_size, n_dim, file=fo)
         for line in output_lines:
             fo.write(line)
 
