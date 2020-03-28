bca7dc1d768461215603699323a54eb7c3ab616a
Masatoshi Suzuki <sappukei.suzuki@gmail.com>
2020-03-28 13:17:36 +0900

    Update script

--- extract_wikipedia2vec_entity_embedding.py
+++ extract_wikipedia2vec_entity_embedding.py
@@ -8,17 +8,12 @@ ENTITY_MARKER = 'ENTITY/'
 
 
 def main(args):
-    output_lines = []
-    with bz2.open(args.input_file, 'rt') as f:
-        vocab_size, n_dim = map(int, f.readline().rstrip('\n').split(' '))
+    with bz2.open(args.input_file, 'rt') as f, \
+         bz2.open(args.output_file, 'wt') as fo:
+        vocab_size, _ = map(int, f.readline().rstrip('\n').split(' '))
         for line in tqdm(f, total=vocab_size):
             if line[:len(ENTITY_MARKER)] == ENTITY_MARKER:
-                output_lines.append(line[len(ENTITY_MARKER):])
-
-    with bz2.open(args.output_file, 'wt') as fo:
-        print(vocab_size, n_dim, file=fo)
-        for line in output_lines:
-            fo.write(line)
+                fo.write(line[len(ENTITY_MARKER):])
 
 
 if __name__ == "__main__":
